f15579a570ade12320a84ab5fafeca6b918d8ac3
moesha08 <moeshaaurelle0@gmail.com>
2025-10-17 01:01:40 -0400

    Trigger redeploy with environment variables

--- config/db.js
+++ config/db.js
@@ -1,28 +1,17 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
-
-let ConnectionString = process.env.MONGODB_URI || "mongodb+srv://moesha:Sorelle1998@cluster003.7lckf8u.mongodb.net/midterm?retryWrites=true&w=majority&appName=Cluster003";
+let ConnectionString = "mongodb+srv://moesha:Sorelle1998@cluster003.7lckf8u.mongodb.net/midterm?retryWrites=true&w=majority&appName=Cluster003"
 
 module.exports = function(){
-    mongoose.connect(ConnectionString)
-        .then(() => {
-            console.log('====> Connected to MongoDB.');
-        })
-        .catch(err => {
-            console.log('MongoDB Connection Warning:', err.message);
-            console.log('Server will continue running without database...');
-        });
+
+    mongoose.connect(ConnectionString);
 
     let mongodb = mongoose.connection;
 
-    mongodb.on('error', (error) => {
-        console.log('MongoDB Connection Error:', error.message);
-        console.log('Server continues running...');
-    });
-    
+    mongodb.on('error', console.error.bind(console, 'Connection Error: '));
     mongodb.once('open', ()=>{
         console.log('====> Connected to MongoDB.');
-    });
+    })
 
     return mongodb;
 }
